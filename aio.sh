39d34f0e19f659fd53f4883ee32c1139b10a9422
Brandon Calhoun <wailrofl@gmail.com>
2016-09-22 16:49:33 -0700

    Update aio.sh

--- aio.sh
+++ aio.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#One-Liner to display  Interface Name, Adapter Branding String, Driver Name/Version, Firmware Version, Bus Location, IPv4 Address and IPv6 Address
+#Displays: Interface Name, Adapter Branding String, Driver Name/Version, Firmware Version, Bus Location, IPv4 Address and IPv6 Address
 
 
-ip link | awk '/mtu/ {gsub(/:/,"");print$2}' | sort | while read -r i; do printf "\n$i\n"; b=$(ethtool -i $i 2> /dev/null | awk 'NR>=1 && NR<=4' );echo "$b" | awk 'NR==4 {print $2}' | xargs lspci -s 2>/dev/null | cut -d' ' -f 4-; echo "$b";  ip addr show dev $i |  awk '/inet/ {print $2}'; done
+ip link | awk '/mtu/ {gsub(/:/,"");print$2}' | sort | while read -r i; do 
+printf "\n$i\n"; b=$(ethtool -i $i 2> /dev/null | awk 'NR>=1 && NR<=4' )
+echo "$b" | awk 'NR==4 {print $2}' | xargs lspci -s 2>/dev/null | cut -d' ' -f 4-
+echo "$b";  ip addr show dev $i |  awk '/inet/ {print $2}'; done
